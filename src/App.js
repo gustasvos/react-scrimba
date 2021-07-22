65a00317ed2a7b00fa8a9ccb5d64f16038eb01c5
Gustavo Rosa <gustavorib.rosa@gmail.com>
2021-07-22 01:07:45 -0300

    aprendendo state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import MainContent from "./components/MainContent"
+// import Header from "./components/Header"
+// import Footer from "./components/Footer"
+// import MainContent from "./components/MainContent"
 
 
 class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            count: 0
+        }
+        // bind do metodo
+
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    // posso colocar os metodos aqui
+
+    handleClick() {
+        this.setState(prevState => {
+            return {
+                count: prevState.count + 1
+            }
+        })
+    }
+
     render() {
-        return (
+        return(
             <div>
-                <Header />
-                <MainContent />
-                <Footer />
+                <h1>{this.state.count}</h1>
+                <button onClick={this.handleClick}>Change!</button>
             </div>
-        );
+        )
     }
 }
 
-export default App;
+export default App
